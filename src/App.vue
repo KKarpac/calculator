dbabbe4c2d599b7785d244eaa3d82d6a92aa2103
Katja KARPAC <katja.karpac@gmail.com>
2020-02-18 11:58:14 +0100

    style it

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
-    <Calculator msg="Calculator"/>
+    <Calculator />
   </div>
 </template>
 
 <script>
 import Calculator from './components/Calculator.vue'
-
 export default {
+  name: 'app',
+  components: {
+    Calculator
+  }
 }
 </script>
 
 <style>
-  .calculator {
-    grid-template-columns: (4, 1fr);
-
-  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
