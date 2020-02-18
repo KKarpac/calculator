91f6716bcd2a2bd8506a1e1e6dce174d64bbe8f1
Katja KARPAC <katja.karpac@gmail.com>
2020-02-18 11:04:59 +0100

    chance to calulator

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Calculator msg="Calculator"/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+}
+</script>
+
+<style>
+  .calculator {
+    grid-template-columns: (4, 1fr);
+
+  }
+</style>
